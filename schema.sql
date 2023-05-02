b22fddaf211309b849f986d4ba3b845d1b1a8d46
shayan1234554321 <kshayan091@gmail.com>
2023-05-03 02:00:17 +0500

    query added and updated animals table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id int,
+    id int not null identity(1,1)
+    primary key(id),
     name varchar(100),
     date_of_birth date,
     escape_attempts int,
     neutered boolean,
     weight_kg real
 );
+
+alter table animals
+add column species varchar(100);
